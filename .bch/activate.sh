2ab21202e2dc4a780b1fb612f9f10f2c49c8396d
Bryan Hann <nobody@nowhere>
2025-12-07 16:54:16 -0330

    Simply activation.
    Remove the [init] folder.
    Have [activate.sh] link on its own.

--- .bch/activate.sh
+++ .bch/activate.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 export BCH_JOTS__root=$(dirname $(dirname $BASH_SOURCE))
-source $(dirname $BASH_SOURCE)/init/fn.sh
-::lbin:: $(dirname $BASH_SOURCE)/lbin
+_link () { ln -s $1 $HOME/.local/bin/$(basename $1); }
+_link $(dirname $BASH_SOURCE)/lbin/bch.jots
+
--- .bch/init/init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source $(dirname $BASH_SOURCE)/fn.sh
-
-for pth in $(ls $(dirname ${BASH_SOURCE[0]})/[0-9]*); do
-    ::dbg:: ". $(basename $pth)"
-    source $pth
-done
-
